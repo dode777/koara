baa65d6f69af84749a451579bcc6abcb2af805b6
Dohae <dode777.dev@gmail.com>
2022-01-04 15:57:00 +0900

    Feat: 로그인 페이지 디자인 작업

--- front/src/App.js
+++ front/src/App.js
@@ -1,7 +1,19 @@
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './pages/Login';
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <>
+      <div>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/login' element={<Login />}></Route>
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </>
+  );
 }
 
 export default App;
